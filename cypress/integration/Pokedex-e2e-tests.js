23951ce8993359879e9650df41b12dbb507c891d
Hachem Tout <htout@GENETEC.COM>
2022-02-09 13:03:25 -0500

    heroku deployment

--- cypress/integration/Pokedex-e2e-tests.js
+++ cypress/integration/Pokedex-e2e-tests.js
@@ -5,8 +5,8 @@ describe('Pokedex', function () {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 
-  it('can navigate to page', function () {
-    cy.visit('http://localhost:5000/pokemon/ivysaur')
-    cy.contains('chlorophyll')
-  })
+  // it('can navigate to page', function () {
+  //   cy.visit('http://localhost:5000/pokemon/weedle')
+  //   cy.contains('chlorophyll')
+  // })
 })
